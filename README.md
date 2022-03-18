862a4a56956a9ba9beeb0bd93b865bebac3d311e
Jesse Garcia <68394599+jgdovis@users.noreply.github.com>
2022-03-17 23:20:45 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Pacman Excercies 
+
+## Description
+Controls the images of multiple PacMan's by simulating their movement in the window and reversing the direction. A web page is created that produces multiple PacMan when a button is clicked. Use JavaScript functions array and techniques to manipulate the Document Object Model or DOM. 
+
+## How to Run
+1. Click `Add PacMan` button to add multiple Pacman to the window. These Pacman start moving from rest (speed 0).
+
+2. Click `Start Game` button to move the pacmans created in the previous step.
+
+3. You can continue adding Pacman by click the `Add PacMan` button. Each click will create another pacman with random initial velocity.
+
+4. If you want to increase the speed of all pacman you can press the `Start Game` button.
+
+5. Refresh the page to make all the PacMen disappear.
+
+## RoadMap of Future Improvement
+- [ ] **Collisions** between Pacman
+- [ ] **Sound** in collisions
+- [ ] **Ghosts** and pacmans can eat them
+- [ ] More **Pacman images** for new diagonal directions
+
+## Licence 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) *The MIT License*
---
+++ pacmen.js
@@ -0,0 +1,80 @@
+var pos = 0;
+const pacArray = [
+  ["./images/PacMan1.png", "./images/PacMan2.png"],
+  ["./images/PacMan3.png", "./images/PacMan4.png"],
+];
+var direction = 0;
+const pacMen = []; 
+var focus = 0;
+
+function setToRandom(scale) {
+  return {
+    x: Math.random() * scale,
+    y: Math.random() * scale,
+  };
+}
+function makePac() {
+
+  let velocity = setToRandom(10); 
+  let position = setToRandom(200);
+  let direction = {
+    x: 0,
+    y: 0,
+  };
+
+  let game = document.getElementById("game");
+  let newimg = document.createElement("img");
+  newimg.style.position = "absolute";
+  newimg.src = pacArray[0][0];
+  newimg.width = 100;
+  newimg.style.left = position.x;
+  newimg.style.top = position.y;
+
+  game.appendChild(newimg);
+
+  return {
+    position,
+    velocity,
+    newimg,
+    direction,
+  };
+}
+setInterval(() => {
+  focus = (focus + 1) % 2;
+}, 100);
+
+function update() {
+  
+  pacMen.forEach((item) => {
+    checkCollisions(item);
+    item.position.x += item.velocity.x;
+    item.position.y += item.velocity.y;
+
+    item.newimg.style.left = item.position.x;
+    item.newimg.style.top = item.position.y;
+    item.newimg.src = pacArray[item.direction.x][focus];
+  });
+  setTimeout(update, 20);
+}
+function checkCollisions(item) {
+
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0
+  ) {
+    item.velocity.x = -item.velocity.x;
+    item.direction.x = item.direction.x === 0 ? 1 : 0;
+  }
+
+  if (
+    item.position.y + item.velocity.y + item.newimg.height >
+      window.innerHeight ||
+    item.position.y + item.velocity.y < 0
+  ) {
+    item.velocity.y = -item.velocity.y;
+ }
+}
+
+function makeOne() {
+  pacMen.push(makePac());
+}
